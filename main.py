b57547389504fc88f26437623572b39e2646163f
Maksim Modestov <maksim.modestov@netcracker.com>
2020-12-09 22:16:54 +0300

    lab2 100%

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import sys
 
 from PIL import Image, ImageDraw
 
-import AESCrypto
+import my_crypto
 import utils
 
 
 def expand_image(small_image):
     factor = 2
-    # small_image.show()
     px = small_image.load()
+    width, height = small_image.size
 
     img = Image.new('RGB', (width * factor + 2, height * factor + 2), 'Black')
     pixels = img.load()
@@ -28,8 +28,6 @@ def expand_image(small_image):
             pixels[factor * i + 1, factor * j + 2] = px[i, j]
             pixels[factor * i + 2, factor * j + 2] = px[i, j]
     img = img.crop((0, 0, fragment_width, fragment_height))
-    img.show()
-    img.save("expand_image.jpg", "JPEG")
     return img
 
 
@@ -42,8 +40,6 @@ def steganography_process(encrypted_fragment):
         v.append(0)
 
     payload_size = len(v) / 8 / 1024.0
-    print("Encrypted payload size: %.3f KB " % payload_size)
-    print("Max payload size: %.3f KB " % max_size)
     print("Коэффициент эффективного использования: %.3f " % float(
         100 * fragment_height * fragment_width / (width * height)))
     if payload_size > max_size - 4:
@@ -64,26 +60,23 @@ def steganography_process(encrypted_fragment):
             data_img.putpixel((w, h), (r, g, b, a))
             idx = idx + 3
 
-    steg_image.save("images/steg_image.png", "PNG")
+    steg_image.save("images/steg_image_racoon.png", "PNG")
     return steg_image
 
 
-def get_image_from_buffer(decrypted_fragment):
+def get_final_image(decrypted_fragment):
     image_stream = io.BytesIO(decrypted_fragment)
     final_image = Image.open(image_stream)
-    final_image.show()
 
     final_image = expand_image(final_image)
 
     x_start = int((width - fragment_width) / 2)
     y_start = int((height - fragment_height) / 2)
 
-    image.show()
     image_copy = image.copy()
     position = (x_start, y_start)
     image_copy.paste(final_image, position)
-    image_copy.save('pasted_image.jpg')
-    image_copy.show()
+    image_copy.save('images/pasted_image_racoon.jpg')
 
     return final_image
 
@@ -92,79 +85,60 @@ def get_encrypted_data(crypto_key, fragment_image):
     buf = io.BytesIO()
     fragment_image.save(buf, format='JPEG')
     byte_im = buf.getvalue()
-
-    encrypt_fragment = AESCrypto.encrypt_image(byte_im, crypto_key)
-
-    # сохраняем зашифрованное изображение
-    with open("encrypt_image.enc", 'wb') as fo:
-        fo.write(encrypt_fragment)
-
+    encrypt_fragment = my_crypto.encrypt_image(byte_im, crypto_key)
     return encrypt_fragment
 
 
 def get_decrypted_data(cryptographic_key, extracting_data):
-    decrypted_fragment = AESCrypto.decrypt_image(extracting_data, cryptographic_key)
-    # сохраняем дешифрованное изображение
-    with open("decrypt_image.dec", 'wb') as fo:
-        fo.write(decrypted_fragment)
-
+    decrypted_fragment = my_crypto.decrypt_image(extracting_data, cryptographic_key)
     return decrypted_fragment
 
 
 def get_fragment_image():
-    #  Шаг 1: выбор фрагмента
     # координаты на оригинальном изображении
     x_start = int((width - fragment_width) / 2)
     y_start = int((height - fragment_height) / 2)
     x_stop = x_start + fragment_width
     y_stop = y_start + fragment_height
 
-    # достаем фрагмент
     fragment_image = image.crop((x_start, y_start, x_stop, y_stop))
-    fragment_image.show()
 
-    # Шаг 2: искажение объекта
     draw = ImageDraw.Draw(image)
     for x in range(x_start, x_stop):
         for y in range(y_start, y_stop):
             draw.point((x, y), (0, 0, 0))
-    image.show()
 
-    # Шаг 3: сжатие объекта
     max_size = (fragment_width / 2, fragment_height / 2)
     fragment_image.thumbnail(max_size, Image.ANTIALIAS)
-    fragment_image.show()
-    # fragment_image.save("thumbnail_image.jpg", "JPEG")
+    fragment_image.save("images/fragment_image_racoon.jpg", "JPEG")
 
     return fragment_image
 
 
 def main_function():
-
     compressed_fragment = get_fragment_image()
 
-    key_pbkdf2 = AESCrypto.get_key_pbkdf2(key)
-
+    key_pbkdf2 = my_crypto.get_key_pbkdf2(key)
+    # шифруем объект
     encrypted_fragment = get_encrypted_data(key_pbkdf2, compressed_fragment)
 
     steg_image = steganography_process(encrypted_fragment)
+
     extracting_data = utils.extract(steg_image)
     # дешифруем
     decrypted_fragment = get_decrypted_data(key_pbkdf2, extracting_data)
     # получаем картинку из дешифрованного фрагмента
-    decrypted_image = get_image_from_buffer(decrypted_fragment)
-    decrypted_image.show()
+    get_final_image(decrypted_fragment)
 
 
 if __name__ == '__main__':
     # оригинальное изображение
-    image_path = "./images/original_squirrel.jpg"
+    image_path = "./images/original_racoon.jpg"
     image = Image.open(image_path)
-    image.show()
     # криптографический ключ
     key = "p@$$w0rd"
 
-    # рассчет размера фрагмента
+    # расчет размера фрагмента
     width, height = image.size
     d = 4  # делитель
     fragment_width = int(width / d)
--- my_crypto.py
+++ my_crypto.py
@@ -9,37 +9,14 @@ def get_key_pbkdf2(key):
     salt = os.urandom(8)  # 64-bit salt
     return PBKDF2(key, salt, 32)  # 256-bit key
 
-
 def encrypt_image(message, key):
     message += b"\0" * (AES.block_size - len(message) % AES.block_size)
     iv = Random.new().read(AES.block_size)
     cipher = AES.new(key, AES.MODE_CBC, iv)
     return iv + cipher.encrypt(message)
 
-
 def decrypt_image(ciphertext, key):
     iv = ciphertext[:AES.block_size]
     cipher = AES.new(key, AES.MODE_CBC, iv)
     plaintext = cipher.decrypt(ciphertext[AES.block_size:])
     return plaintext.rstrip(b"\0")
-
-
-##########
-def encrypt_image_and_save(file_name, key):
-    with open(file_name, 'rb') as fo:
-        plaintext = fo.read()
-    enc = encrypt(plaintext, key)
-    with open(file_name + ".enc", 'wb') as fo:
-        fo.write(enc)
-
-
-def decrypt_image_and_save(self, file_name, key):
-    with open(file_name, 'rb') as fo:
-        ciphertext = fo.read()
-    dec = self.decrypt(ciphertext, key)
-    with open(file_name + "decr", 'wb') as fo:
-        fo.write(dec)
-
-
-def clear(self):
-    lambda: os.system('cls')
