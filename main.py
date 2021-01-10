7afd7b66c1ec9ba5883d814b7e5b83f51922744a
Maksim Modestov <maksim.modestov@netcracker.com>
2021-01-10 13:38:39 +0300

    lab4

--- main.py
+++ main.py
@@ -1,65 +1,66 @@
-import math
+from __future__ import division
+import datetime
+import hashlib
+import time
 
-from PIL import Image
-
-import myimagehash
-import image_slicer
-from stegano import lsb
-
-
-def hamming_distance(string1, string2):
-    distance = 0
-    length = len(string1)
-    for i in range(length):
-        if string1[i] != string2[i]:
-            distance += 1
-    return distance
+from dateutil import rrule
+from Crypto.Cipher import AES
+import multiprocessing
+from joblib import Parallel, delayed
+from tqdm import tqdm
+import numpy as np
+import sys
 
 
-def main_function(original_image_filename, deformed_image_filename):
-    original_image = Image.open(original_image_filename)
-    deformed_image = Image.open(deformed_image_filename)
-    # расчет количества блоков
-    n = 64
-    width, height = original_image.size
-    col = math.ceil(width / n)
-    row = math.ceil(height / n)
+def int_overflow(val):
+    if not -sys.maxsize - 1 <= val <= sys.maxsize:
+        val = (val + (sys.maxsize + 1)) % (2 * (sys.maxsize + 1)) - sys.maxsize - 1
+    return val
 
-    # пороговая величина
-    hash_length = 64
-    threshold = hash_length * 0.1
 
-    # разбитие изображений на блоки
-    orig_img_tiles = image_slicer.slice(original_image_filename, col=col, row=row, save=False)
-    deform_img_tiles = image_slicer.slice(deformed_image_filename, col=col, row=row, save=False)
+def rand():
+    global next_int
+    next_int = int_overflow(next_int * 1103515245 + 12345)
+    return np.int_((next_int / 65536) % 32768)
 
-    for orig_block, deform_block in zip(orig_img_tiles, deform_img_tiles):
-        # getting block hash
-        orig_hash = myimagehash.phash_simple(orig_block.image)
-        deform_hash = myimagehash.phash_simple(deform_block.image)
 
-        # hide a message(hash) in images with the LSB
-        orig_block.image = lsb.hide(orig_block.image, str(orig_hash))
-        deform_block.image = lsb.hide(deform_block.image, str(deform_hash))
+def s_rand(seed):
+    global next_int
+    next_int = seed
 
-        # Find a message(hash) in images with the LSB
-        decoded_orig_hash = lsb.reveal(orig_block.image.copy())
-        decoded_deform_hash = lsb.reveal(deform_block.image.copy())
 
-        if hamming_distance(decoded_orig_hash, decoded_deform_hash) > threshold:
-            deform_block.image = deform_block.image.convert("L")
+def seed_iteration(seed):
+    unite = int(time.mktime(seed.timetuple()))
+    s_rand(unite)
+    rand_1 = rand()
+    rand_2 = rand()
+    preKey = abs(rand_1 << 16) | rand_2
 
-    result_image = image_slicer.join(deform_img_tiles, width, height)
-    result_image.save("./images/result.png")
+    hashing = hashlib.md5(str(preKey).encode('utf-8'))
 
-    original_image.show()
-    deformed_image.show()
-    result_image.show()
+    key = hashing.digest()
+    cipher = AES.new(key, AES.MODE_CBC, saltHashing)
+
+    tmp = cipher.decrypt(encrypted).hex()
+    if tmp.startswith('ffd8ffe0'):
+        data = bytes.fromhex(tmp)
+        open('image.jpg', 'wb').write(data)
+
+        print('\nKey is: ' + str(key.hex()))
+        print('Encryption date is: ' + str(seed))
+        print("Assembling time: {:.2f}s".format(time.time() - start_time))
+
+        sys.exit()
 
 
 if __name__ == '__main__':
-    original_image_path = "./images/original_racoon.jpg"
-    deformed_image_path = "./images/deformed_racoon.jpg"
-    # original_image_path = "./images/original_squirrel.jpg"
-    # deformed_image_path = "./images/deformed_squirrel.jpg"
-    main_function(original_image_path, deformed_image_path)
+    seed_min = datetime.datetime.strptime(str('2020-Nov-01 00:00:00'), '%Y-%b-%d %H:%M:%S')
+    seed_max = datetime.datetime.strptime(str('2020-Dec-01 00:00:00'), '%Y-%b-%d %H:%M:%S')
+
+    encrypted = open('./encrypted', 'rb').read()
+    inputs = tqdm(rrule.rrule(rrule.SECONDLY, dtstart=seed_min, until=seed_max))
+
+    start_time = time.time()
+    saltHashing = hashlib.md5(b' ').digest()
+    next_int = np.int_(1)
+    Parallel(n_jobs=multiprocessing.cpu_count())(delayed(seed_iteration)(i) for i in inputs)
